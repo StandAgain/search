261fccf12f5f1ca2fc32c10f2348c10c5825d38f
StandAgain <standagaincontact@gmail.com>
2026-04-06 09:30:43 +1000

    Update search-index.js

--- search-index.js
+++ search-index.js
@@ -1,1 +1,14 @@
-
+var SEARCH_INDEX = [
+  {
+    url: 'https://www.standagain.com.au/',
+    title: 'Stand Again | Coercive Control & Domestic Violence Support for Men',
+    snippet: 'Support for male victims of coercive control and domestic violence in Australia.',
+    keywords: 'coercive control domestic violence male victims support Australia'
+  },
+  {
+    url: 'https://www.standagain.com.au/stage-2-survive/tools-to-survive/',
+    title: 'Grey Rock, JADE & Coercive Control Survival Tools for Men',
+    snippet: 'Practical tools including Grey Rock and JADE for men surviving coercive control.',
+    keywords: 'grey rock JADE coercive control tools survive'
+  }
+];
